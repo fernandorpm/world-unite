af60d1ae0151b275d6b0c5e8a05d43359cbfd74f
Fernando R P Marques <frpm1001@gmail.com>
2021-05-25 18:55:45 -0300

    Add article and article_category validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :author_id, :title, :text, :image, presence: true
+  validates :title, length: { in: 8..100 }
+  validates :text, length: { in: 50..2000 }
 
   belongs_to :author, foreign_key: 'author_id', class_name: 'User'
 
--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,4 +1,6 @@
 class ArticleCategory < ApplicationRecord
+  validates :article_id, :category_id, presence: true
+
   belongs_to :article
   belongs_to :category
 end
