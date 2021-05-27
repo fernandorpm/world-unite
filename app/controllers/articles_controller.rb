77df090d832b5975d4ee8e80a0b2c1d823100a4c
Fernando R P Marques <frpm1001@gmail.com>
2021-05-27 13:31:28 -0300

    Update views with errors display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -64,6 +64,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:user_id, :title, :text, :image)
+      params.require(:article).permit(:author_id, :title, :text, :image)
     end
 end
