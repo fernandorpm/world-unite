c9eb014dad93a67caf53707b7a7f8cd46857a1c0
Fernando Marques <frpm1001@gmail.com>
2021-06-03 13:29:42 -0300

    Update migration for easier set-up

--- db/migrate/20210525173716_create_categories.rb
+++ db/migrate/20210525173716_create_categories.rb
@@ -6,5 +6,12 @@ class CreateCategories < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+
+    Category.create!(name: 'The Americas', priority: 1)
+    Category.create!(name: 'Africa', priority: 2)
+    Category.create!(name: 'Asia', priority: 3)
+    Category.create!(name: 'Europe', priority: 4)
+    Category.create!(name: 'Oceania', priority: 5)
+    Category.create!(name: 'Antarctica', priority: 6)
   end
 end
